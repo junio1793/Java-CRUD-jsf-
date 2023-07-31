b6d58eb1b4cfcdea5eff732cf4fe78997506bc99
unknown <sjunior863@gmail.com>
2023-07-31 19:54:25 -0300

    primeiro commit

---
+++ src/test/java/com/crudjsf/GestaoFesta/GestaoFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudjsf.GestaoFesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
